e52e90d3f5a45b96516cb3d84e03a3264d994116
Sébastien Touzé <contact@sebastien-touze.fr>
2020-12-06 12:19:06 +0100

    Day3 part2.

--- day3.py
+++ day3.py
@@ -13,32 +13,43 @@ def load_matrix():
             line_size = len(line)
             break
 
-    res = [['*']*line_size for  i in range(nb_lines)]
+    result = [['*']*line_size for i in range(nb_lines)]
     i = 0
     with open("data/day3input", "rt") as file:
         for line in file:
             line = line.strip()
             for j in range(0, len(line)):
-                res[i][j] = line[j]
+                result[i][j] = line[j]
             i += 1
 
-    return res, nb_lines
+    return result, nb_lines
 
 
-def move(matrix, x, y):
-    x = (x + 3) % len(matrix[x])
-    y += 1
+def move(matrix, slope, x, y):
+    x = (x + slope[0]) % len(matrix[x])
+    y += slope[1]
     return x, y, '#' == matrix[y][x]
 
 
+def test_slope(matrix, matrix_end, slope_to_test):
+    current_x, current_y = 0, 0
+    encountered_trees = 0
+    while current_y < matrix_end - 1:
+        current_x, current_y, on_a_tree = move(matrix, slope_to_test, current_x, current_y)
+        if on_a_tree:
+            encountered_trees += 1
+    return encountered_trees
+
+
 # Initial position
-current_x, current_y = 0, 0
-encountered_trees = 0
 matrix, destination = load_matrix()
 
-while current_y < destination - 1:
-    current_x, current_y, on_a_tree = move(matrix, current_x, current_y)
-    if on_a_tree:
-        encountered_trees += 1
+slopes = ([1, 1], [3, 1], [5, 1], [7, 1], [1, 2])
 
-print(encountered_trees)
+solution = 1
+for s in slopes:
+    res = test_slope(matrix, destination, s)
+    print("For slope right " + str(s[0]) + " and down " + str(s[1]) + ", result is " + str(res) + " trees encountered")
+    solution = solution * res
+
+print("Final result : " + str(solution))
