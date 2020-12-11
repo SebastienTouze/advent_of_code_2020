fa320cef30cb4b633cbf9460cf09a1910a9bb1f1
Sébastien Touzé <contact@sebastien-touze.fr>
2020-12-11 07:25:19 +0100

    Day6, part 2.
    Python use reference copies not values copy for list!

--- day6.py
+++ day6.py
@@ -2,7 +2,7 @@
 # Script for Advent Of Code 2020
 # DAY 06
 
-# We could do a trick here using a dictionary to keep unique keys then back to strin, but it's king of python magic,
+# We could do a trick here using a dictionary to keep unique keys then back to string, but it's king of python magic,
 # let's keep things an algorithmic way
 def delete_duplicate_letters(s):
     source = []
@@ -17,7 +17,15 @@ def delete_duplicate_letters(s):
     return res
 
 
-def parse_input_print_result(file):
+def keep_common_letters(ref_list, new_list):
+    res = ref_list.copy()  # /!\ because Python affectation is reference affectation Youhou =.=
+    for char in ref_list:
+        if char not in new_list:
+            res.remove(char)
+    return res
+
+
+def parse_input_print_result_p1(file):
     with open(file, "rt") as file:
         answers_concat = ""
         total = 0
@@ -31,8 +39,38 @@ def parse_input_print_result(file):
         total += len(delete_duplicate_letters(answers_concat))
         print(total)
 
-print ("** Test (expected: 11) **")
-parse_input_print_result("data/day6test")
 
-print ("** Real **")
-parse_input_print_result("data/day6input")
+def parse_input_print_result_p2(file):
+    with open(file, "rt") as file:
+        possible_answers = []
+        new_group = True
+        total = 0
+        for line in file:
+            line = line.strip()
+            if len(line) > 0:
+                if new_group:
+                    new_group = False
+                    possible_answers[:] = line
+                else:
+                    line_char_list = []
+                    line_char_list[:] = line
+                    possible_answers = keep_common_letters(possible_answers, line_char_list)
+            else:
+                total += len(possible_answers)
+                possible_answers = []
+                new_group = True
+        total += len(possible_answers)
+        print(total)
+
+
+print("** Test (part 1, expected: 11) **")
+parse_input_print_result_p1("data/day6test")
+
+print("** Real **")
+parse_input_print_result_p1("data/day6input")
+
+print("** Test (part 2, expected: 6) **")
+parse_input_print_result_p2("data/day6test")
+
+print("** Real **")
+parse_input_print_result_p2("data/day6input")
